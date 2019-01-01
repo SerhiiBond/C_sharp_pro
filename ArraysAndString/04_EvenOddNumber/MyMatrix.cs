fd29e7d51839f9cedb6a78780ff9bb83c60096f9
SerhiiBond <46292623+SerhiiBond@users.noreply.github.com>
2019-01-01 14:44:09 +0200

    v1_04
    начато решение задачи 4

---
+++ ArraysAndString/04_EvenOddNumber/MyMatrix.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_EvenOddNumber
+{
+    class MyMatrix
+    {
+        Random rand = new Random();
+        int[] arr;
+        public MyMatrix(int length)
+        {
+            arr = new int[length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+               arr[i] = rand.Next(10, 99);
+            }
+        }
+
+        public int this[int index]
+        {
+            get { return arr[index]; }
+        }
+
+        public int NumberOfElement()
+        {
+            return arr.Length;
+        }
+        
+        public void Show()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine(););
+        }
+        public void SortArr()
+        {
+            int tmp = ;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                tmp = arr[i];
+                for (int j = 0; j < arr.Length; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ ArraysAndString/04_EvenOddNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 4. Задан одномерный массив целых чисел. 
+ Образуйте из него два отсортированных по возрастанию массива, 
+ содержащих четные и нечетные числа. Подсказка: четное число делится на 2 без остатка.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_EvenOddNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов массива: ");
+            int n = Convert.ToInt16(Console.ReadLine());
+
+            MyMatrix arr = new MyMatrix(n);
+            arr.Show();
+            Console.WriteLine(new string('-',100));
+
+            Console.ReadKey();
+        }
+    }
+}
