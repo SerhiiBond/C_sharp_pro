1ab2be9eced4d370c24d05c9b9e82738fd221e05
SerhiiBond <46292623+SerhiiBond@users.noreply.github.com>
2019-01-01 10:49:59 +0200

    v1 ex01
    v1; массивы и строки; задача 01.

---
+++ ArraysAndString/01_SymbolsInLine/Handler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_SymbolsInLine
+{
+    class Handler
+    {
+        String str;
+        public Handler(string str)
+        {
+            this.str = str;
+        }
+
+        public int CountingChar(char ch)
+        {
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (ch == str[i])
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ ArraysAndString/01_SymbolsInLine/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 1. Введите с клавиатуры строку произвольной длины и подсчитайте процент вхождения заданного символа в строку.*/
+using System;
+
+namespace _01_SymbolsInLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string str = Console.ReadLine();
+            Console.WriteLine(new String('-',100));
+            Console.WriteLine("Вы ввели: {0}",str);
+
+            Console.Write("Введите символ, который нужно найти:");
+            char ch = Convert.ToChar(Console.ReadLine());
+            Handler handler = new Handler(str);
+            int count = handler.CountingChar(ch);
+            if (count != 0)
+            {
+                Console.WriteLine("Символ {0} в данной строке повторяется {1} раз", ch, count);
+            }
+            else
+            {
+                Console.WriteLine("Символа {0} в данной строке нет", ch, count);
+            }
+           
+           
+            Console.ReadKey();
+        }
+    }
+}
