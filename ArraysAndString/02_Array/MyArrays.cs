cc3ebd2a1a0dc2fb5dcd952b42eb453a81b1a767
SerhiiBond <46292623+SerhiiBond@users.noreply.github.com>
2019-01-01 10:53:09 +0200

    v1 ex02
    v1; массивы и строки; задача 02.

---
+++ ArraysAndString/02_Array/MyArrays.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Array
+{
+    
+    class MyArrays
+    {
+        int[][] myArray;
+        int row, line;
+        public MyArrays(int line, int row)
+        {
+            this.row = row;
+            this.line = line;
+            myArray = new int[line][];
+            for (int i = 0; i < line; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    myArray[i] = new int[row];
+                }
+            }
+            CreateArrays();
+        }
+
+        private void CreateArrays()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                for (int j = 0; j < myArray[i].Length; j++)
+                {
+                    myArray[i][j] = rand.Next(10, 99);
+                }
+            }
+        }
+        public void ShowArrays()
+        {
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                for (int j = 0; j < myArray[i].Length; j++)
+                {
+                    Console.Write(myArray[i][j]+"  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ArraysAndString/02_Array/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 2. Задан массив действительных чисел размерности 10х10.
+ Найти суммы элементов каждой строки, произведения элементов каждого столбца, 
+ и максимальный элемент главной диагонали(подсказка: все элементы, для которых номер строки совпадает с номером столбца).*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArrays array = new MyArrays(10, 10);
+            array.ShowArrays();
+
+            Console.ReadKey();
+        }
+    }
+}
